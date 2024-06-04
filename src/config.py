8aa19e51bbafb976365a67b0fc1f5db274bc57ce
koen-benne <koen.benne@gmail.com>
2024-06-04 13:19:04 +0200

    update output file name

--- src/config.py
+++ src/config.py
@@ -16,4 +16,4 @@ OPPONENT = "Ajax"
 API_KEY = os.getenv("DEEPGRAM_API_KEY")
 MIN_TRANSCRIPT_LENGTH = 6
 DEBUG_TRANSCRIPTION = False
-OUTPUT_FILE_NAME = "output/Feyenoord-" + OPPONENT + "-" + str(datetime.today()) +".yaml"
+OUTPUT_FILE_NAME = "output/Feyenoord-" + OPPONENT + "_" + str(datetime.today()).split()[0] +".yaml"

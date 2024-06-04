53ece5e6d8106ecde9ff6c553cb5bba682c2b41f
koen-benne <koen.benne@gmail.com>
2024-06-04 12:33:51 +0200

    change decision prompt

--- rf-voice-to-blog/blogger.py
+++ rf-voice-to-blog/blogger.py
@@ -1,16 +1,6 @@
-from gpt4all import GPT4All
 from openai import OpenAI
 import os
 from config import OPPONENT
-
-def writeBlog(transcription):
-    model = GPT4All("wizardlm-13b-v1.2.Q4_0.gguf")
-
-    response = model.generate(prompt=transcription + "\n\nDit is een automatisch transcript van een Feyenoord-Ajax wedstrijd. Mogelijk kloppen sommige woorden en interpunctie niet. Verbeter de tekst.",
-        temp=0)
-    print(response)
-
-
 
 def writeBlogGPT(transcription):
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -33,13 +23,12 @@ def writeBlogGPT(transcription):
         messages=[
             {
                 "role": "user",
-                "content": "'" + corrected_transcript + "'\n\nBeschrijft dit een cruciaal moment van de wedstrijd? Antwoord alleen ja of nee."
+                "content": "'" + corrected_transcript + "'\n\nBeschrijft dit een relevant moment van de wedstrijd? Antwoord alleen ja of nee."
             }
         ],
         model="gpt-3.5-turbo",
     )
 
-    print(chat_completion.choices[0].message.content.lower())
     if "ja" in chat_completion.choices[0].message.content.lower():
         chat_completion = client.chat.completions.create(
             messages=[
